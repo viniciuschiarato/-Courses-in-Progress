573fd48b8797033823b8432a41331aa93e4a4f47
Vinícius Chiarato <viniciussantoschiarato@gmail.com>
2022-05-20 11:40:02 -0300

    ex102 v2

--- Cursos/Python/PythonExercicios/ex102.py
+++ Cursos/Python/PythonExercicios/ex102.py
@@ -1,7 +1,14 @@
-def fatorial(n, show=False):
+def fatorial(n=1, show=False):
     f = 1
     for c in range(n, 0, -1):
         f *= c
-    if show:
+        print(f'{c}', end=' ')
+        if c > 1:
+            print('x', end=' ')
+        if c == 1:
+            print('=', end=' ')
+    print(f)
+
+
 num = int(input('Calculo de fatorial\n\nDigite o número para saber seu fatorial: '))
 fatorial(num, show=False)
