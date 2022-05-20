cec38995a63430e8a840babe8797d4f8c058949b
Vinícius Chiarato <viniciussantoschiarato@gmail.com>
2022-05-20 10:50:07 -0300

    ex101 v2

--- Cursos/Python/PythonExercicios/ex101.py
+++ Cursos/Python/PythonExercicios/ex101.py
@@ -1,20 +1,18 @@
-from datetime import date
-
-
+#
 def voto(nasc):
+    resp = 0
     from datetime import date
     idade = date.today().year - nasc
     if idade < 16:
         resp = 'VOTO NEGADO'
     if 65 > idade >= 18:
         resp = 'VOTO OBRIGATÓRIO'
-    if 16 < idade < 18:
+    if 16 <= idade < 18:
         resp = 'VOTO OPCIONAL'
     if idade >= 65:
         resp = 'VOTO OPCIONAL'
-    return resp
+    return print(f'Com {idade} anos: {resp}')
 
 
 input_born = int(input('Em que ano você nasceu? '))
 voto(input_born)
-print(f'Com {date.today().year - input_born} anos: {voto(input_born)}')
