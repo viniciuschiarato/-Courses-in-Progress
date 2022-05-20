03c74348bbc25cfb50aa1b6deeec2ff0b9b60d65
Vinícius Chiarato <viniciussantoschiarato@gmail.com>
2022-05-20 13:04:02 -0300

    ex101 ex102

--- Cursos/Python/PythonExercicios/ex101.py
+++ Cursos/Python/PythonExercicios/ex101.py
@@ -1,5 +1,7 @@
-#
-def voto(nasc):
+# Exercício Python 101: Crie um programa que tenha uma função chamada voto() que vai receber
+# como parâmetro o ano de nascimento de uma pessoa, retornando um valor literal indicando
+# se uma pessoa tem voto NEGADO, OPCIONAL e OBRIGATÓRIO nas eleições.
+"""def voto(nasc):
     resp = 0
     from datetime import date
     idade = date.today().year - nasc
@@ -15,4 +17,19 @@ def voto(nasc):
 
 
 input_born = int(input('Em que ano você nasceu? '))
-voto(input_born)
+voto(input_born)"""
+
+
+def voto(nasc):
+    from datetime import date
+    idade = date.today().year - nasc
+    if idade < 16:
+        return f'Com {idade} anos: NÃO VOTA.'
+    elif 16 <= idade < 18 or idade > 65:
+        return f'Com {idade} anos: VOTO OPICIONAL.'
+    else:
+        return f'Com {idade} anos: VOTO OBRIGATÓRIO.'
+
+
+input_born = int(input('Em que ano você nasceu? '))
+print(voto(input_born))
--- Cursos/Python/PythonExercicios/ex102.py
+++ Cursos/Python/PythonExercicios/ex102.py
@@ -1,14 +1,21 @@
 def fatorial(n=1, show=False):
+    """
+    -> Calcule o fatorial de um número
+    :param n: O número a ser calculado
+    :param show: (opcional) Mostrar ou não a memória de cálculo.
+    :return: O valor do fatorial de um número n.
+    """
     f = 1
     for c in range(n, 0, -1):
+        if show:
+            print(f'{c}', end=' ')
+            if c > 1:
+                print('x', end=' ')
+            if c == 1:
+                print('=', end=' ')
         f *= c
-        print(f'{c}', end=' ')
-        if c > 1:
-            print('x', end=' ')
-        if c == 1:
-            print('=', end=' ')
     print(f)
 
 
-num = int(input('Calculo de fatorial\n\nDigite o número para saber seu fatorial: '))
-fatorial(num, show=False)
+num = int(input('Digite o número para saber seu fatorial: '))
+fatorial(num)
