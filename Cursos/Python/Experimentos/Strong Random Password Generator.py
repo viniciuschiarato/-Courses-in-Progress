108e060cea9c5f470952f1e1c598cf75bc9d5002
Vinícius Chiarato <viniciussantoschiarato@gmail.com>
2022-05-24 18:41:45 -0300

    ex003 html

--- Cursos/Python/Experimentos/Strong Random Password Generator.py
+++
@@ -1,8 +0,0 @@
-import string
-from random import choice
-database = string.ascii_letters + string.punctuation + string.digits
-length = int(input('Characters length: '))
-password = ''
-for v in range(0, length):
-    password += choice(database)
-print(password)
