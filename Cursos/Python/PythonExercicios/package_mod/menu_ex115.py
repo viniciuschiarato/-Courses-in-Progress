894a6f31006eeeaa8334c6afcc7f48cdda13bce8
Vinícius Chiarato <viniciussantoschiarato@gmail.com>
2022-05-27 12:22:34 -0300

    menu_ex115 v2

--- Cursos/Python/PythonExercicios/package_mod/menu_ex115.py
+++ Cursos/Python/PythonExercicios/package_mod/menu_ex115.py
@@ -14,29 +14,44 @@ def lc(color=0):
 
 
 def menu():
-    print('-'*30)
-    print('MENU PRINCIPAL'.center(30))
-    print('-'*30)
-    print(f'{lc(3)}1 - {lc(4)}Ver pessoas cadastradas')
-    print(f'{lc(3)}2 - {lc(4)}Cadastrar novas pessoas')
-    print(f'{lc(3)}3 - {lc(4)}Sair do sistema        ')
-    print(f'{lc(0)}-' * 30)
     while True:
-        try:
-
-            option = int(input('Sua opção: '))
-            if option not in range(1, 3):
-                option = 'erro'
-            int(option)
-        except(ValueError, TypeError):
-            print(f'{lc(1)}Erro! A opção informada é invalida.{lc()}')
-            continue
-        except KeyboardInterrupt:
-            print(f'{lc(1)}Erro! Nenhuma opção informada.{lc()}')
-            option = 3
-            return option
+        option = ''
+        print('-'*30)
+        print('MAIN MENU'.center(30))
+        print('-'*30)
+        print(f'{lc(3)}1 - {lc(4)}Ver pessoas cadastradas')
+        print(f'{lc(3)}2 - {lc(4)}Cadastrar novas pessoas')
+        print(f'{lc(3)}3 - {lc(4)}Sair do sistema        ')
+        print(f'{lc(0)}-' * 30)
+        while True:
+            try:
+                option = int(input('Sua opção: '))
+                if option not in range(1, 4):
+                    option = 'erro'
+                int(option)
+                break
+            except(ValueError, TypeError):
+                print(f'{lc(1)}Erro! A opção informada é invalida.{lc()}')
+                continue
+            except KeyboardInterrupt:
+                print(f'{lc(1)}Nenhuma opção informada.{lc()}')
+                option = 3
+                break
+        if option == 1:
+            print('-' * 30)
+            print('OPÇÃO 1'.center(30))
+            print('-' * 30)
+        if option == 2:
+            print('-' * 30)
+            print('OPÇÃO 2'.center(30))
+            print('-' * 30)
         else:
-            return option
+            print('-' * 30)
+            print('THANK YOU!'.center(30))
+            print('COME BACK SOON!'.center(30))
+            print('-' * 30)
+            break
 
 
-print(menu())
+menu()
+
