2b3a4381e61502009899198ff6db4422be79d6d6
Vinícius Chiarato <viniciussantoschiarato@gmail.com>
2022-06-13 12:27:06 -0300

    Update ex013

---
+++ Cursos/JavaScript/exercicios/ex013/ex013.js
@@ -0,0 +1,30 @@
+var lib_date = new Date()
+var dia_semana = lib_date.getDay()
+console.log(dia_semana)
+
+switch(dia_semana){
+    case 0:
+        console.log('Domingo')
+        break
+    case 1:
+        console.log('Segunda-Feira')
+        break
+    case 2:
+        console.log('Terça-Feira')
+        break
+    case 3:
+        console.log('Quarta-Feira')
+        break
+    case 4:
+        console.log('Quinta-Feira')
+        break
+    case 5:
+        console.log('Sexta-Feira')
+        break
+    case 6:
+        console.log('Sabado')
+        break
+    default:
+        console.log('[ERRO] Dia inválido!')
+        break
+}
