a4983d79c8a6264c18dc4892a95f7633115bd581
Vinícius Chiarato <105664186+viniciuschiarato@users.noreply.github.com>
2022-06-15 09:39:30 -0300

    Update

--- Cursos/JavaScript/exercicios/ex014/script.js
+++ Cursos/JavaScript/exercicios/ex014/script.js
@@ -1,19 +1,22 @@
 lib_date = new Date
-hora = lib_date.getHours()
-//hora = 15
+hours = lib_date.getHours()
+minutes = lib_date.getMinutes()
+seconds = lib_date.getSeconds()
 sec_modified = document.getElementsByTagName('section')[0]
 body_modified = document.getElementsByTagName('body')[0]
-sec_modified.innerHTML = `<h3>Agora são ${hora} horas</h3>`
+sec_modified.innerHTML = `<h3>${hours}:${minutes}:${seconds}</h3>`
 
-if (hora > 0 && hora < 6 || hora > 18) {
+if (hours > 0 && hours < 6 || hours > 18) {
     sec_modified.innerHTML += `<img src="imagens/modified/noite.png" alt="Noite">`
     body_modified.style.background = '#01021b'
     sec_modified.style.background = '#000000b6'
 }
-else if (hora < 12) {
+
+else if (hours < 12) {
     sec_modified.innerHTML += `<img src="imagens/modified/manha.png" alt="Amanhecer">`
     body_modified.style.background = '#5fc5c5c7'
 }
+
 else {
     sec_modified.innerHTML += `<img src="imagens/modified/entardecer.png" alt="Entardecer">`
     body_modified.style.background = '#c56c30e0'
