2a8f9d7ab291beb5093542f3d476818986ac3fef
Vinícius Chiarato <105664186+viniciuschiarato@users.noreply.github.com>
2022-06-17 08:17:21 -0300

    Add ex017

---
+++ Cursos/JavaScript/exercicios/ex016/ex016.js
@@ -0,0 +1,19 @@
+// Estrutura de repetição com teste lógico no início:
+/*var c = 1
+while (c <= 6){
+    console.log(`Passo ${c}`)
+    c++
+}*/
+// Estrutura de repetição com teste lógico no final:
+/*var c = 1
+do{
+    console.log(`Passo ${c}`)
+    c++
+}while(c <= 6)
+*/
+//for(inicio; teste; incremento)
+console.log('Vai começar...')
+for(var c = 1; c <= 6; c++){
+    console.log(c)
+}
+console.log('Fim!')
