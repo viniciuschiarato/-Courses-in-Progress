774c5eb76ecf2f0ca5c7e2ba10b45b8a2443e5a8
Vinícius Chiarato <105664186+viniciuschiarato@users.noreply.github.com>
2022-06-17 10:35:24 -0300

    Update ex016

--- Cursos/JavaScript/exercicios/ex016/script.js
+++ Cursos/JavaScript/exercicios/ex016/script.js
@@ -1,19 +1,30 @@
-function contar(){
+function contar() {
     inicio = (Number(document.getElementById('inicio').value))
     fim = (Number(document.getElementById('fim').value))
     passo = (Number(document.getElementById('passo').value))
     resultado = document.getElementById('resultado')
     resultado.innerHTML = 'Contando:<br>'
-    c = 0
-    for(inicio; inicio <= fim; inicio += passo){
-        c++
-        if (c == 1){
-            resultado.innerHTML += `${inicio}`
-        }
-        else{
-            resultado.innerHTML += ` → ${inicio}`
-        }
-        
+    if (inicio == '' || fim == '') {
+        resultado.innerHTML = 'Impossível contar.'
     }
-    resultado.innerHTML += '🚩'
+    else if (inicio > fim){
+        alert('[ERRO] Início não pode ser menor que o fim.')
+    }
+    else {
+        if (passo == 0){
+            alert('[ERRO] Passo inválido! Considerando passo = 1.')
+            passo = 1
+        }
+        c = 0
+        for (inicio; inicio <= fim; inicio += passo) {
+            c++
+            if (c == 1) {
+                resultado.innerHTML += `${inicio}`
+            }
+            else {
+                resultado.innerHTML += ` → ${inicio}`
+            }
+        }
+        resultado.innerHTML += '🚩'
+    }
 }
