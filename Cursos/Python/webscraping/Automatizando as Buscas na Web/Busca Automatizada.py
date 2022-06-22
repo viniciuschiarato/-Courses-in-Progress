47b9962d6e6e8dca13506270a602b359bb830d0b
Vinícius Chiarato <105664186+viniciuschiarato@users.noreply.github.com>
2022-06-22 17:17:18 -0300

    add

--- Cursos/Python/webscraping/Automatizando as Buscas na Web/Busca Automatizada.py
+++ Cursos/Python/webscraping/Automatizando as Buscas na Web/Busca Automatizada.py
@@ -1,22 +1,38 @@
 import requests
 from bs4 import BeautifulSoup
+import pandas as pd
+
+lista_noticias = list()
+
 response = requests.get('https://g1.globo.com/')
 
-content = response.content  # formato "bytes"
+content = response.content
 
-site = BeautifulSoup(content, 'html.parser')  # [1] formato do conteúdo
+site = BeautifulSoup(content, 'html.parser')
 
 # HTML da notícia
-noticia = site.find('div', attrs={'class': "feed-post-body"})  # attrs atributos html | find encontra o primeiro atributo com a especificação passada
+noticias = site.findAll('div', attrs={'class': "feed-post-body"})
 
-#print(noticia.prettify())  # prettify melhora a visualização
+# validação
+for noticia in noticias:
 
-# Título
-titulo = noticia.find('a', attrs={"class": "feed-post-link gui-color-primary gui-color-hover"})
+    # Título
+    titulo = noticia.find('a', attrs={"class": "feed-post-link gui-color-primary gui-color-hover"})
 
-#print(titulo)
+    #print(titulo.text)
+    #print(titulo['href'])
 
-# Subtítilo
-subtitulo = noticia.find('a', attrs={"class": "gui-color-primary gui-color-hover feed-post-body-title bstn-relatedtext"})
+    # Subtítilo
+    subtitulo = noticia.find('a', attrs={"class": "gui-color-primary gui-color-hover feed-post-body-title "
+                                                  "bstn-relatedtext"})
 
-print(subtitulo.text)
+    if (subtitulo):
+        lista_noticias.append([titulo.text, titulo['href'], subtitulo])
+    else:
+        lista_noticias.append([titulo.text, titulo['href'], ''])
+
+news = pd.DataFrame(lista_noticias, columns=['Título', 'Link', 'Subtítulo'])
+
+news.to_excel('noticias.xlsx', index=False)
+
+print(news)
