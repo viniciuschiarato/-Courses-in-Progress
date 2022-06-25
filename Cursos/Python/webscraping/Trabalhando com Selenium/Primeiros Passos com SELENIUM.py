f0d2550f472aaca30a1f366f3854d0ebdab2e6d5
Vinícius Chiarato <105664186+viniciuschiarato@users.noreply.github.com>
2022-06-25 18:07:57 -0300

    add

--- Cursos/Python/webscraping/Trabalhando com Selenium/Primeiros Passos com SELENIUM.py
+++ Cursos/Python/webscraping/Trabalhando com Selenium/Primeiros Passos com SELENIUM.py
@@ -1,6 +1,6 @@
 from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
 from time import sleep
 
 
@@ -8,9 +8,7 @@ browser = webdriver.Chrome()  # método
 
 browser.get('https://www.google.com.br/')
 
-sleep(2)
-
-browser.find_element_by_xpath('/html/body/div[1]/div[3]/form/div[1]/div[1]/div[1]/div/div[2]/input').send_keys(
+browser.find_element(By.XPATH, '/html/body/div[1]/div[3]/form/div[1]/div[1]/div[1]/div/div[2]/input').send_keys(
     'tears for fears everybody wants to rule the world')
 
 browser.find_element_by_xpath('/html/body/div[1]/div[3]/form/div[1]/div[1]/div[1]/div/div[2]/input').send_keys(
--- Cursos/Python/webscraping/Trabalhando com Selenium/teste.py
+++ Cursos/Python/webscraping/Trabalhando com Selenium/teste.py
@@ -1,3 +1,36 @@
 import requests
 from bs4 import BeautifulSoup
-from selenium import
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
+from time import sleep
+
+options = Options()
+# options.add_argument('--headless')
+options.add_argument('window-size=400,800')
+
+browser = webdriver.Chrome(options=options)
+
+browser.get('https://www.youtube.com/')
+
+sleep(2)
+
+input_place = browser.find_element(By.XPATH,'//*[@id="search"]')
+
+input_place.send_keys('feel good')
+
+input_place.submit()
+
+
+
+
+
+
+
+
+# site = BeautifulSoup(browser.page_source, "html.parser")
+
+# print(site.prettify())
+
+# sleep(30)
