34434fd6f32ec54403d231f756251df45ae8394e
Vinícius Chiarato <105664186+viniciuschiarato@users.noreply.github.com>
2022-06-25 12:49:06 -0300

    add

--- Cursos/Python/webscraping/Trabalhando com Selenium/Primeiros Passos com SELENIUM.py
+++ Cursos/Python/webscraping/Trabalhando com Selenium/Primeiros Passos com SELENIUM.py
@@ -1,8 +1,18 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 from time import sleep
+
 
 browser = webdriver.Chrome()  # método
 
-browser.get()
+browser.get('https://www.google.com.br/')
+
+sleep(2)
+
+browser.find_element_by_xpath('/html/body/div[1]/div[3]/form/div[1]/div[1]/div[1]/div/div[2]/input').send_keys(
+    'tears for fears everybody wants to rule the world')
+
+browser.find_element_by_xpath('/html/body/div[1]/div[3]/form/div[1]/div[1]/div[1]/div/div[2]/input').send_keys(
+    Keys.ENTER)
 
 
