8b5e8b5cc21e3b2486490daa356594447e54c4bd
Vinícius Chiarato <105664186+viniciuschiarato@users.noreply.github.com>
2022-06-26 08:21:23 -0300

    add

--- Cursos/JavaScript/exercicios/ex017/script.js
+++ Cursos/JavaScript/exercicios/ex017/script.js
@@ -1,5 +1,18 @@
 function tabuada(){
     numero = Number(document.getElementById('entrada').value)
     resultado = document.getElementById('resultado')
-    
+    if (numero == ''){
+        alert('Por favor, digite um número!')
+    }
+    else{
+        var c = 1
+        while (c <= 10){
+            produto = numero * c
+            if (c == 1){
+                resultado.innerHTML = '</td>' + numero + ' x ' + c + ' = ' + produto + '</td><br>'}
+            else{
+            resultado.innerHTML += '</td>' + numero + ' x ' + c + ' = ' + produto + '<br>'}
+            c++
+        }
+    }
 }
