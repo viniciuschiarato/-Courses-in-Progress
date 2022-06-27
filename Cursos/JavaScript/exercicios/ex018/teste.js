a3654c65e354be2d08077edec7067fe9ed9a9418
Vinícius Chiarato <105664186+viniciuschiarato@users.noreply.github.com>
2022-06-27 14:54:01 -0300

    Update

--- Cursos/JavaScript/exercicios/ex018/teste.js
+++ Cursos/JavaScript/exercicios/ex018/teste.js
@@ -1,16 +1,29 @@
-let valores = [8, 1, 7, 4, 2, 9]
+//let valores = [8, 1, 7, 4, 2, 9]
 //valores.sort()
-console.log(valores)
+//console.log(valores)
 
 /*
 for (pos = 0; pos < valores.length; pos++) {
     console.log(`A posição ${pos} tem o valor ${valores[pos]}`)
 }
 */
-for(let pos in valores) {
+/*for(let pos in valores) {
     console.log(`A posição ${pos} tem o valor ${valores[pos]}`)
-}
+}*/
 num = [4, 5, 6, 7, 8]
 
-console.log(num.indexOf(3))  // return "3" the key
-console.log(num.indexOf(3))  // return "-1" not found
+//console.log(num.indexOf(3))  // return "3" the key
+//console.log(num.indexOf(3))  // return "-1" not found
+function myArrayMax(num) {
+    let len = num.length;
+    let max = -Infinity;
+    while (len--) {
+      if (num[len] > max) {
+        max = num[len];
+      }
+    }
+    return console.log(max);
+}
+myArrayMax(num)
+
+console.log(Math.max.apply(null, num))
