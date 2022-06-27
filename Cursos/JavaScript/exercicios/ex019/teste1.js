896bc98f80d5e2c0171621cdb2a7ae370c33451e
Vinícius Chiarato <105664186+viniciuschiarato@users.noreply.github.com>
2022-06-27 08:22:51 -0300

    add

---
+++ Cursos/JavaScript/exercicios/ex019/teste1.js
@@ -0,0 +1,13 @@
+function par_impar(n) {
+    if (n % 2 == 0) {
+        return 'par'
+    }
+    else {
+        return 'ímpar'
+    }
+}
+
+// console.log(par_impar(11))
+
+let resp = par_impar(11)
+console.log(resp)
