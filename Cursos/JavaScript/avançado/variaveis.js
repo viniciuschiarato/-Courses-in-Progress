d3961f3ba044f3e2ec15f02e2ecd9b319ad73bfc
Vinícius Chiarato <105664186+viniciuschiarato@users.noreply.github.com>
2022-12-26 08:37:24 -0300

    Add

---
+++ Cursos/JavaScript/avançado/variaveis.js
@@ -0,0 +1,9 @@
+// Não posso criar variáveis com paçavras reservadas
+// Variáveis precisam ter nomes significativos
+// Não é possível declarar uma variável iniciando o nome dela com um número
+// Não pode conter espaços ou traços
+// Melhor forma de nomear uma variavel com um nome composto é com camelCase
+// Case-sensitive (maiusculas e minúsculas fazem diferença)
+// Não é possivel redeclarar variáveis criadas com LET
+// Não é possivel alterar o valor contido por uma variável do tipo CONST
+// VAR está em desuso usa-se LET ou  CONST
