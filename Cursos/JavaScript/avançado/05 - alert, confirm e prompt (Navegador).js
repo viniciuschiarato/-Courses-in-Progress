979bdb5de123caef6b9666b103f2e600f612cfb3
Vinícius Chiarato <105664186+viniciuschiarato@users.noreply.github.com>
2023-01-23 18:47:53 -0300

    add

---
+++ Cursos/JavaScript/avançado/05 - alert, confirm e prompt (Navegador).js
@@ -0,0 +1,5 @@
+// feito no console do navegador
+// Esses três métodos estão contidos no objeto window (objetos c métodos)
+
+// alert('Olá Mundo') retorna undefined não apontando para local nenhum na memória
+// confirm('esta de acordo?') retorna true ou false --> capturando com: let variavel = confirm('esta de acordo?')   
